431282bd6df8c5e990d248a28433253a0e572517
newwebash <newtona2011@gmail..com>
2022-02-23 22:04:05 +0000

    Render both username and favorite movie

--- src/components/list.js
+++ src/components/list.js
@@ -5,12 +5,14 @@ class UsersList extends Component {
     return (
       <div>
       <p>{this.props.users[1].userName}</p>
+<p>{this.props.movies[1].name}</p>
     	<ul>
       		{
             	this.props.profiles.map((profile) => (
       				<li key={profile.id}>
-						<p>{this.props.users[profile.userID].userName}</p>
-						{profile.userID}
+						<p>Username: {this.props.users[profile.userID].userName}</p>
+						<p>Favorite movie: {this.props.movies[profile.favoriteMovieID].name}</p>
+						{profile.favoriteMovieID}
       				</li>
     			))
             }
